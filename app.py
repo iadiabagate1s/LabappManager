cd9f8e29dbe152e7a6a7cea0e195e77848923d2f
Isaac Diabagate <iadiabagate1s@semo.edu>
2020-08-10 06:18:49 -0400

    db add

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from forms import LoginForm, Register, Tasksform, Projform, Edituser
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///labmanager'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgres:///labmanager')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY']=os.environ.get('SECRET_KEY','malachixxl')
